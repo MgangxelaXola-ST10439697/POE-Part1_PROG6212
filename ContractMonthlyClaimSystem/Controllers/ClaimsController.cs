e3d76b203222db0ca9c94050f358d2029f955899
MgangxelaXola-ST10439697 <164156933+MgangxelaXola-ST10439697@users.noreply.github.com>
2025-09-17 14:09:06 +0200

    commit 2

---
+++ ContractMonthlyClaimSystem/Controllers/ClaimsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSystem.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        
+            public IActionResult Submit() {
+            return View(); 
+        }
+        public IActionResult History() { 
+            return View();
+        }
+    }
+    }
+
---
+++ ContractMonthlyClaimSystem/Controllers/DocumentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSystem.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        public IActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
